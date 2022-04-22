bc06c29e18430c4b7068b0e95df93fadb082360c
genndy007 <kochevgenazp@gmail.com>
2022-04-22 21:31:48 +0300

    Add installed apps

--- wb_app/src/main/settings.py
+++ wb_app/src/main/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
