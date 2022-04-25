2de76668e1ca272070469d7967429dd8ae7a6803
genndy007 <kochevgenazp@gmail.com>
2022-04-25 17:02:22 +0300

    Create card serializer to not show user_id

---
+++ wb_app/src/cards/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Card, Record
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ['id', 'articul', 'brand', 'goods_name', 'price_without_discount', 'price_with_discount', 'supplier']
+
