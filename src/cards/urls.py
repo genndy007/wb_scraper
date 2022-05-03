19247578ff492e6bf55daa4020bd01a4267f5962
genndy007 <kochevgenazp@gmail.com>
2022-05-04 00:12:25 +0300

    Make urls end with slash

--- src/cards/urls.py
+++ src/cards/urls.py
@@ -3,7 +3,7 @@ from .views import AllCardsView, SingleCardView, CardStatsView, UpdateInfoView
 
 urlpatterns = [
     path('', AllCardsView.as_view()),
-    path('<int:pk>', SingleCardView.as_view()),
-    path('<int:pk>/stats', CardStatsView.as_view()),
-    path('update', UpdateInfoView.as_view()),
+    path('<int:pk>/', SingleCardView.as_view()),
+    path('<int:pk>/stats/', CardStatsView.as_view()),
+    path('update/', UpdateInfoView.as_view()),
 ]
