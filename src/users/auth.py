b4f492d71e4c0633e230dd6f6639cdae8af41991
genndy007 <kochevgenazp@gmail.com>
2022-05-03 21:32:52 +0300

    Move auth file to users, it is used only for users

---
+++ src/users/auth.py
@@ -0,0 +1,47 @@
+import datetime
+from django.conf import settings
+from rest_framework.exceptions import AuthenticationFailed
+import jwt
+
+from users.models import User
+
+
+def validate_login_password(login: str, password: str):
+    if not login or not password:
+        raise AuthenticationFailed('Insufficient credentials, need login and password')
+
+    user = User.objects.filter(login=login).first()
+    if user is None:
+        raise AuthenticationFailed('User not found')
+
+    if not user.check_password(password):
+        raise AuthenticationFailed('Incorrect password')
+
+    return user
+
+
+def generate_jwt_token(user) -> str:
+    time_now = datetime.datetime.utcnow()
+    payload = {
+        'id': user.id,
+        'exp': time_now + datetime.timedelta(minutes=60),
+        'iat': time_now,
+    }
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    return token
+
+
+def is_jwt_authenticated(request):
+    token = request.COOKIES.get('jwt')
+    if not token:
+        raise AuthenticationFailed('You need to authenticate first - login')
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256', ])
+    except:
+        raise AuthenticationFailed('You need to authenticate first - login')
+
+    return payload
+
+
+
