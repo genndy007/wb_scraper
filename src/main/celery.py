7d8d40a9a4ad037b7c9bbb044661254dbe2dbfbe
genndy007 <kochevgenazp@gmail.com>
2022-05-03 17:41:06 +0300

    Add rabbitmq connection string

--- src/main/celery.py
+++ src/main/celery.py
@@ -5,6 +5,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
+
 celery_app = Celery('main')
 
 # Using a string here means the worker doesn't have to serialize
